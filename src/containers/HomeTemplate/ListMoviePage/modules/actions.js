c2121fb201f04f7885fa9a25fe78203a41e5757c
dpnguyen53 <dpnguyen53@gmail.com>
2021-10-01 17:37:55 +0700

    upgrade interceptors

--- src/containers/HomeTemplate/ListMoviePage/modules/actions.js
+++ src/containers/HomeTemplate/ListMoviePage/modules/actions.js
@@ -1,43 +1,46 @@
 import * as ActionType from "./constants";
 import axios from "axios";
+import api from "./../../../../utils/apiUtils";
 
 export const actFetchListMovie = () => {
-    //Gọi axios trong action
-    return (dispatch) => {
-        //request
-        dispatch(actListMovieRequest());
-        axios({
-            url:
-                "https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01",
-            method: "GET",
-        })
-            .then((result) => {
-                //success
-                dispatch(actListMovieSuccess(result.data));
-            })
-            .catch((err) => {
-                //failed
-                dispatch(actListMovieFailed(err));
-            });
-    };
+  //Gọi axios trong action
+  return (dispatch) => {
+    //request
+    dispatch(actListMovieRequest());
+    // axios({
+    //     url:
+    //         "http://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01",
+    //     method: "GET",
+    // })
+    api
+      .get("QuanLyPhim/LayDanhSachPhim?maNhom=GP01")
+      .then((result) => {
+        //success
+        dispatch(actListMovieSuccess(result.data));
+      })
+      .catch((err) => {
+        //failed
+        dispatch(actListMovieFailed(err));
+      });
+  };
 };
 
 const actListMovieRequest = () => {
-    return {
-        type: ActionType.LIST_MOVIE_REQUEST,
-    };
+  return {
+    type: ActionType.LIST_MOVIE_REQUEST,
+  };
 };
 
 const actListMovieSuccess = (data) => {
-    return {
-        type: ActionType.LIST_MOVIE_SUCCESS,
-        payload: data,
-    };
+  return {
+    type: ActionType.LIST_MOVIE_SUCCESS,
+    payload: data,
+  };
 };
 
 const actListMovieFailed = (data) => {
-    return {
-        type: ActionType.LIST_MOVIE_FAILED,
-        payload: data,
-    };
+  return {
+    type: ActionType.LIST_MOVIE_FAILED,
+    payload: data,
+  };
 };
--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,5 +1,23 @@
 import axios from "axios";
 
-export default axios.create({
+export const TOKEN_CYBERSOFT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAxMCIsIkhldEhhblN0cmluZyI6IjAzLzAzLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY0NjI2NTYwMDAwMCIsIm5iZiI6MTYxNjc3ODAwMCwiZXhwIjoxNjQ2NDEzMjAwfQ.-kBpd2bu-Gx60Sgo7peCQ2ASUYeOSuQB3Z9NWxIyAw8";
+
+const api = axios.create({
   baseURL: "https://movie0706.cybersoft.edu.vn/api/",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    config.headers = {
+      ...config.headers,
+      TokenCyberSoft: TOKEN_CYBERSOFT,
+    };
+    return config;
+  },
+  (errors) => {
+    return Promise.reject(errors);
+  }
+);
+
+export default api;
